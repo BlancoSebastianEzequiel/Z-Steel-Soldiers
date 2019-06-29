213b5b2f243f76abd556cf8fe05be92b2aeffdda
sebastian <sebastian.e.blanco@gmail.com>
2019-06-29 13:41:06 -0300

    compile latex reports

--- scripts/make_report.sh
+++ scripts/make_report.sh
@@ -1,1 +1,19 @@
 #!/usr/bin/env bash
+
+make_report () {
+    cd docs/$1
+    pdflatex report.tex
+    pdflatex report.tex
+    rm report.aux
+    rm report.log
+    rm report.toc
+    rm report.out
+    mv report.pdf $1.pdf
+    cd ..
+    cd ..
+    clear
+}
+
+make_report project_manual
+make_report technical_documentation
+make_report user_manual
