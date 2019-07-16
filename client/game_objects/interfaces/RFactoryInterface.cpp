7a8840358264949ce6aeef74978b4aad426a878e
sebastian <sebastian.e.blanco@gmail.com>
2019-07-16 15:30:24 -0300

    use createButton passng type parameter

--- client/game_objects/interfaces/RFactoryInterface.cpp
+++ client/game_objects/interfaces/RFactoryInterface.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
 #include "RFactoryInterface.h"
 #include "buildingInterface.h"
-#include "../buttons/createGruntButton.h"
-#include "../buttons/createLaserButton.h"
-#include "../buttons/createPsychoButton.h"
-#include "../buttons/createPyroButton.h"
-#include "../buttons/createToughButton.h"
-#include "../buttons/createSniperButton.h"
+#include "../buttons/createButton.h"
+#include "../../../libs/definitions.h"
 //------------------------------------------------------------------------------
 // CLIENT R FACTORY INTERFACE CONSTRUCTOR
 //------------------------------------------------------------------------------
@@ -16,28 +12,21 @@ RFactoryInterface::RFactoryInterface(
         BuildingInterface(id, pos_x, pos_y, tech_level) {
     current_frame = new Frame("Buildings/Interface/robot_factory_menu.png");
     if (tech_level >= 1) {
-        // Grunt
-        buttons.push_back(new CreateGruntButton(id, pos_x, pos_y + 20));
+        buttons.push_back(new CreateButton(id, pos_x, pos_y + 20, GRUNT));
     }
     if (tech_level >= 2) {
-        // Psycho y Tough
-        buttons.push_back(new CreatePsychoButton(
-                id, pos_x + 55, pos_y + 20));
-        buttons.push_back(new CreateToughButton(id, pos_x, pos_y + 40));
+        buttons.push_back(new CreateButton(id, pos_x + 55, pos_y + 20, PSYCHO));
+        buttons.push_back(new CreateButton(id, pos_x, pos_y + 40, TOUGH));
     }
     if (tech_level >= 3) {
-        // Sniper
-        buttons.push_back(new CreateSniperButton(
-                id, pos_x + 55, pos_y + 40));
+        buttons.push_back(new CreateButton(id, pos_x + 55, pos_y + 40, SNIPER));
     }
     if (tech_level >= 4) {
-        // Pyro
-        buttons.push_back(new CreatePyroButton(id, pos_x, pos_y + 60));
+        buttons.push_back(new CreateButton(id, pos_x, pos_y + 60, PYRO));
     }
     if (tech_level >= 5) {
-        // Laser
-        buttons.push_back(new CreateLaserButton(
-                id, pos_x + 55, pos_y + 60));
+        buttons.push_back(
+                new CreateButton(id, pos_x + 55, pos_y + 60, ROBOT_LASER));
     }
 }
 //------------------------------------------------------------------------------
--- client/game_objects/interfaces/fortInterface.cpp
+++ client/game_objects/interfaces/fortInterface.cpp
@@ -1,55 +1,38 @@
 #include <iostream>
 #include "fortInterface.h"
 #include "buildingInterface.h"
-#include "../buttons/createGruntButton.h"
-#include "../buttons/createLaserButton.h"
-#include "../buttons/createPsychoButton.h"
-#include "../buttons/createPyroButton.h"
-#include "../buttons/createToughButton.h"
-#include "../buttons/createSniperButton.h"
-#include "../buttons/createLightTankButton.h"
-#include "../buttons/createMediumTankButton.h"
-#include "../buttons/createJeepButton.h"
-#include "../buttons/createHeavyTankButton.h"
-#include "../buttons/createMMLButton.h"
-//------------------------------------------------------------------------------
-// CLIENT FORTINTERFACE CONSTRUCTOR
-//------------------------------------------------------------------------------
+#include "../buttons/createButton.h"
+#include "../../../libs/definitions.h"
+
 FortInterface::FortInterface(
         unsigned int id, unsigned int pos_x, unsigned int pos_y,
         unsigned int tech_level):
         BuildingInterface(id, pos_x, pos_y, tech_level) {
     current_frame = new Frame("Buildings/Interface/fort_menu.png");
     if (tech_level >= 1) {
-        // Jeep y Grunt
-        buttons.push_back(new CreateGruntButton(id, pos_x, pos_y + 20));
-        buttons.push_back(
-                new CreateJeepButton(id, pos_x + 55, pos_y + 20));
+        buttons.push_back(new CreateButton(id, pos_x, pos_y + 20, GRUNT));
+        buttons.push_back(new CreateButton(id, pos_x + 55, pos_y + 20, JEEP));
     }
     if (tech_level >= 2) {
-        // Lighttank, Psycho y Tough
         buttons.push_back(
-                new CreateLightTankButton(id, pos_x + 55, pos_y + 40));
-        buttons.push_back(new CreatePsychoButton(id, pos_x , pos_y + 40));
-        buttons.push_back(new CreateToughButton(id, pos_x, pos_y + 60));
+                new CreateButton(id, pos_x + 55, pos_y + 40, LIGHT_TANK));
+        buttons.push_back(new CreateButton(id, pos_x , pos_y + 40, PSYCHO));
+        buttons.push_back(new CreateButton(id, pos_x, pos_y + 60, TOUGH));
     }
     if (tech_level >= 3) {
-        // Medium tank y Sniper
          buttons.push_back(
-                 new CreateMediumTankButton(id, pos_x + 55, pos_y + 60));
-         buttons.push_back(new CreateSniperButton(id, pos_x, pos_y + 80));
+                 new CreateButton(id, pos_x + 55, pos_y + 60, MEDIUM_TANK));
+         buttons.push_back(new CreateButton(id, pos_x, pos_y + 80, SNIPER));
     }
     if (tech_level >= 4) {
-        // HeavyTank y Pyro
          buttons.push_back(
-                 new CreateHeavyTankButton(id, pos_x + 55, pos_y + 80));
-         buttons.push_back(new CreatePyroButton(id, pos_x, pos_y + 100));
+                 new CreateButton(id, pos_x + 55, pos_y + 80, HEAVY_TANK));
+         buttons.push_back(new CreateButton(id, pos_x, pos_y + 100, PYRO));
     }
     if (tech_level >= 5) {
-        // MML y Laser
         buttons.push_back(
-                new CreateMMLButton(id, pos_x + 55, pos_y + 100));
-        buttons.push_back(new CreateLaserButton(id, pos_x, pos_y + 120));
+                new CreateButton(id, pos_x + 55, pos_y + 100, MML));
+        buttons.push_back(
+                new CreateButton(id, pos_x, pos_y + 120, ROBOT_LASER));
     }
 }
-//------------------------------------------------------------------------------
