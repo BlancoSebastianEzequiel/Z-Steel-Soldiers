7325ac90342d9c3909594517898dd193db5c7c4b
sebastian <sebastian.e.blanco@gmail.com>
2019-07-20 20:17:52 -0300

    refactor building object. Now there are only object with different behaviors

--- server/territories/territories.cpp
+++ server/territories/territories.cpp
@@ -1,52 +1,34 @@
-// "Copyright [2017] <Copyright SebastianBlanco>"
-//------------------------------------------------------------------------------
 #include <vector>
 #include "territories.h"
 #include "../terrains/node.h"
 #include "../objects/object.h"
 #include "../players/player.h"
-#include "../objects/flags/flag.h"
-//------------------------------------------------------------------------------
-//------------------------------------------------------------------------------
-// SERVER TERRAIN CONSTRUCTOR
-//------------------------------------------------------------------------------
+
 Territories::Territories(const size_t id): id(id) {
     player = nullptr;
 }
-//------------------------------------------------------------------------------
-// SERVER TERRAIN DESTRUCTOR
-//------------------------------------------------------------------------------
+
 Territories::~Territories() {}
-//------------------------------------------------------------------------------
-// ADD NODE
-//------------------------------------------------------------------------------
+
 void Territories::addNode(Node &aNode) {
     aNode.addTerritory(*this);
     nodes.push_back(&aNode);
 }
-//------------------------------------------------------------------------------
-// GET NODES
-//------------------------------------------------------------------------------
+
 std::vector<Node*>& Territories::getNodes(){
     return nodes;
 }
-//------------------------------------------------------------------------------
-// GET ID
-//------------------------------------------------------------------------------
+
 const size_t& Territories::getId() const {
     return id;
 }
-//------------------------------------------------------------------------------
-// ADD PLAYER
-//------------------------------------------------------------------------------
+
 void Territories::changePlayer(Player& aPlayer) {
     if (hasAPlayer()) player->deleteTerritory(id);
     player = &aPlayer;
     player->addTerritory(*this);
 }
-//------------------------------------------------------------------------------
-// HAS A FORT
-//------------------------------------------------------------------------------
+
 bool Territories::hasAFort() const {
     for (const Node* aNode : nodes) {
         if (!aNode->hasAGroundObject()) continue;
@@ -55,27 +37,21 @@ bool Territories::hasAFort() const {
     }
     return false;
 }
-//------------------------------------------------------------------------------
-// GET BUILDING
-//------------------------------------------------------------------------------
-Buildings* Territories::getBuilding() const {
+
+Object* Territories::getBuilding() const {
     for (const Node* aNode : nodes) {
         if (!aNode->hasAGroundObject()) continue;
         if (!aNode->getGroundObject()->isFort()) continue;
-        return (Buildings*)aNode->getGroundObject();
+        auto* object = (Object*) aNode->getGroundObject();
+        return object;
     }
     return nullptr;
 }
-//------------------------------------------------------------------------------
-//  HAS A PLAYER
-//------------------------------------------------------------------------------
+
 bool Territories::hasAPlayer() const {
     return player != nullptr;
 }
-//------------------------------------------------------------------------------
-// GET PLAYER
-//------------------------------------------------------------------------------
+
 const Player* Territories::getOwner() const {
     return player;
 }
-//------------------------------------------------------------------------------
