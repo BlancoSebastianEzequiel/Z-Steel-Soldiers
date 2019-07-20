bc784133b7675d3a13947c1082c7fe0d96224427
sebastian <sebastian.e.blanco@gmail.com>
2019-07-20 20:14:07 -0300

    add template method to read object, armament and units

--- server/settings/settings.cpp
+++ server/settings/settings.cpp
@@ -1,42 +1,19 @@
-// "Copyright [2017] <Copyright SebastianBlanco>"
-//------------------------------------------------------------------------------
 #include "settings.h"
 #include <boost/property_tree/ptree.hpp>
 #include "../../libs/definitions.h"
-//------------------------------------------------------------------------------
-// SETTINGS CONSTRUCTOR
-//------------------------------------------------------------------------------
-Settings::Settings() {
-    namespace pt = boost::property_tree;
-    pt::ptree data = aParser.readJson("settings.json");
-    for (pt::ptree::value_type& value: data.get_child("units")) {
-        auto* robot = new unit_t();
-        for (pt::ptree::value_type& field: value.second.get_child("")) {
-            (*robot)(field.first, field.second.get<float>(""));
-        }
-        units((size_t) (*robot)["name"], robot);
-    }
 
-    for (pt::ptree::value_type& value: data.get_child("armament")) {
-        auto* armament = new armament_t();
-        for (pt::ptree::value_type& field: value.second.get_child("")) {
-            (*armament)(field.first, field.second.get<size_t>(""));
-        }
-        armaments((*armament)["id"], armament);
-    }
+Settings::Settings() {
+    data = aParser.readJson("settings.json");
+    buildDict<unit_t, units_t, float>("units", units);
+    buildDict<armament_t, armaments_t, size_t>("armament", armaments);
+    buildDict<object_t, objects_t, float>("objects", objects);
 
     EarthPrairieSnow("groundFactor", 1);
     WaterSwamp("groundFactor", 0.7);
     RoadAsphaltRoad("groundFactor", 1.5);
     Lava("groundFactor", 0);
-
-    StoneIceBlocks("structurePoints", 10);
-    Buildings("structurePoints", 1000);  // 1000
-    ConcreteAndWoodenBridges("structurePoints", 1000);  // 1000
 }
-//------------------------------------------------------------------------------
-// SETTINGS DESTRUCTOR
-//------------------------------------------------------------------------------
+
 Settings::~Settings() {
     for (unit_t* aUnit: units.getList()) {
         delete aUnit;
@@ -44,5 +21,18 @@ Settings::~Settings() {
     for (armament_t* anArmament: armaments.getList()) {
         delete anArmament;
     }
+    for (object_t* anObject: objects.getList()) {
+        delete anObject;
+    }
 }
-//------------------------------------------------------------------------------
+
+template<class T, class V, class type>
+void Settings::buildDict(const std::string &name, V& dicts) {
+    for (pt::ptree::value_type& value: data.get_child(name)) {
+        T* dict = new T();
+        for (pt::ptree::value_type& field: value.second.get_child("")) {
+            (*dict)(field.first, field.second.get<type>(""));
+        }
+        dicts((size_t) (*dict)["id"], dict);
+    }
+}
--- server/settings/settings.h
+++ server/settings/settings.h
@@ -1,12 +1,10 @@
-// "Copyright [2017] <Copyright SebastianBlanco>"
-//------------------------------------------------------------------------------
 #ifndef SETTINGS_H
 #define SETTINGS_H
-//------------------------------------------------------------------------------
+
 #include <string>
 #include "../../libs/Dicc.h"
 #include "../../libs/Parser.h"
-//------------------------------------------------------------------------------
+
 typedef Dicc<std::string, size_t> armament_t;
 typedef Dicc<size_t, armament_t*> armaments_t;
 
@@ -14,27 +12,28 @@ typedef Dicc<std::string, float> unit_t;
 typedef Dicc<size_t, unit_t*> units_t;
 
 typedef Dicc<std::string, float> object_t;
-//------------------------------------------------------------------------------
+typedef Dicc<size_t, object_t*> objects_t;
+
+namespace pt = boost::property_tree;
+
 class Settings {
  public :
     Parser aParser;
+    pt::ptree data;
+
     units_t units;
     armaments_t armaments;
+    objects_t objects;
 
     object_t EarthPrairieSnow;
     object_t WaterSwamp;
     object_t RoadAsphaltRoad;
     object_t Lava;
-
-    object_t ConcreteAndWoodenBridges;
-    object_t StoneIceBlocks;
-    object_t Buildings;
  public :
-    //--------------------------------------------------------------------------
     Settings();
-    //--------------------------------------------------------------------------
     ~Settings();
-    //--------------------------------------------------------------------------
+    template<class T, class V, class type>
+    void buildDict(const std::string &name, V& dicts);
 };
-//------------------------------------------------------------------------------
+
 #endif  // SETTINGS_H
