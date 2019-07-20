3d2c4cc67602059e3f2a9f6029b87076dcf932b9
sebastian <sebastian.e.blanco@gmail.com>
2019-07-20 20:19:47 -0300

    refactor objects creation by reducing method only to one.

--- server/maps/map.cpp
+++ server/maps/map.cpp
@@ -1,17 +1,13 @@
-// "Copyright [2017] <Copyright SebastianBlanco>"
-//------------------------------------------------------------------------------
 #include <vector>
 #include <string>
 #include <iostream>
 #include "map.h"
 #include "../../libs/Exception.h"
-#include "../terrains/node.h"
-#include "../terrains/lava.h"
 #include "../settings/settings.h"
-#include "../objects/solids/solids.h"
-#include "../objects/flags/flag.h"
 #include "../players/player.h"
 #include "../territories/territories.h"
+#include "../terrains/node.h"
+#include "../terrains/lava.h"
 #include "../terrains/earth_prairie_snow/dust.h"
 #include "../terrains/earth_prairie_snow/grass.h"
 #include "../terrains/road_asphalt_road/road.h"
@@ -19,19 +15,11 @@
 #include "../terrains/water_swamp/water.h"
 #include "../terrains/water_swamp/swamp.h"
 #include "../terrains/earth_prairie_snow/snow.h"
-#include "../objects/buildings/fort.h"
-#include "../objects/buildings/robotFactory.h"
-#include "../objects/buildings/vehicleFactory.h"
-#include "../objects/bridges/woodenBridge.h"
-#include "../objects/bridges/concreteBridge.h"
-#include "../objects/solids/iceBlock.h"
-#include "../objects/solids/stone.h"
+#include "../objects/object.h"
 #include "../../libs/definitions.h"
-//------------------------------------------------------------------------------
+
 extern Settings settings;
-//------------------------------------------------------------------------------
-// MAP CONSTRUCTOR
-//------------------------------------------------------------------------------
+
 Map::Map(char* mapFileName) {
     maximumQuantityOfPlayers = 0;
     objectsQuant = 0;
@@ -56,9 +44,7 @@ Map::Map(char* mapFileName) {
         if (command == TERRITORY) addNodesToTerritory(line, aTerritory);
     }
 }
-//------------------------------------------------------------------------------
-// MAP DESTRUCTOR
-//------------------------------------------------------------------------------
+
 Map::~Map() {
     for (uint32_t x = 0; x < width; x++) {
         for (uint32_t y = 0; y < height; y++) {
@@ -72,31 +58,18 @@ Map::~Map() {
         delete anObject;
     }
 }
-//------------------------------------------------------------------------------
-// GET NODE
-//------------------------------------------------------------------------------
+
 Node* Map::getNode(uint32_t x, uint32_t y) const {
     if (!belongs(x, y)) throw Exception("(%u, %u) does not belong\n", x, y);
     return gameMap[x][y];
 }
-//------------------------------------------------------------------------------
-// BELONGS
-//------------------------------------------------------------------------------
+
 bool Map::belongs(uint32_t x, uint32_t y) const {
     return x >= 0 && x < width && y >= 0 && y < height;
 }
-//------------------------------------------------------------------------------
-// BELONGS
-//------------------------------------------------------------------------------
-bool Map::belongs(const Node& aNode) const {
-    return belongs(aNode.x, aNode.y);
-}
-//------------------------------------------------------------------------------
-// GET ADJACENT OF
-//------------------------------------------------------------------------------
+
 void Map::addAdjacentTo(Node &aNode) const {
     std::vector<Node*> adjacent;
-    //--------------------------------------------------------------------------
     if (belongs(aNode.x-1, aNode.y-1)) {
         adjacent.push_back(getNode(aNode.x-1, aNode.y-1));
     }
@@ -106,7 +79,6 @@ void Map::addAdjacentTo(Node &aNode) const {
     if (belongs(aNode.x-1, aNode.y+1)) {
         adjacent.push_back(getNode(aNode.x-1, aNode.y+1));
     }
-    //--------------------------------------------------------------------------
     if (belongs(aNode.x+1, aNode.y-1)) {
         adjacent.push_back(getNode(aNode.x+1, aNode.y-1));
     }
@@ -116,61 +88,31 @@ void Map::addAdjacentTo(Node &aNode) const {
     if (belongs(aNode.x+1, aNode.y+1)) {
         adjacent.push_back(getNode(aNode.x+1, aNode.y+1));
     }
-    //--------------------------------------------------------------------------
     if (belongs(aNode.x, aNode.y-1)) {
         adjacent.push_back(getNode(aNode.x, aNode.y-1));
     }
     if (belongs(aNode.x, aNode.y+1)) {
         adjacent.push_back(getNode(aNode.x, aNode.y+1));
     }
-    //--------------------------------------------------------------------------
     aNode.adjacent = adjacent;
 }
-//------------------------------------------------------------------------------
-// GET OBJECTS
-//------------------------------------------------------------------------------
+
 objectsMap& Map::getObjects() {
     return objects;
 }
-//------------------------------------------------------------------------------
-// GET OBJECT
-//------------------------------------------------------------------------------
+
 Object* Map::getObject(size_t id) {
     return objects[id];
 }
-//------------------------------------------------------------------------------
-// GET BUILDING
-//------------------------------------------------------------------------------
+
 Buildings* Map::getBuilding(size_t id) {
     return buildings[id];
 }
-//------------------------------------------------------------------------------
-// GET BRIDGE
-//------------------------------------------------------------------------------
-Bridges* Map::getBridge(size_t id) {
-    return bridges[id];
-}
-//------------------------------------------------------------------------------
-// GET SOLID
-//------------------------------------------------------------------------------
-Solids* Map::getSolid(size_t id) {
-    return solids[id];
-}
-//------------------------------------------------------------------------------
-// GET FLAG
-//------------------------------------------------------------------------------
-Flag* Map::getFlag(size_t id) {
-    return flags[id];
-}
-//------------------------------------------------------------------------------
-// GET TERRAIN
-//------------------------------------------------------------------------------
+
 Territories* Map::getTerritory(size_t id) {
     return territories[id];
 }
-//------------------------------------------------------------------------------
-// ADD INITIAL TERRAIN TO PLAYER
-//------------------------------------------------------------------------------
+
 void Map::addInitialTerritoryToPlayer(Player& aPlayer) {
     for (Territories* aTerritory: territories.getList()) {
         if (!aTerritory->hasAFort()) continue;
@@ -181,190 +123,38 @@ void Map::addInitialTerritoryToPlayer(Player& aPlayer) {
     size_t playerId = aPlayer.getId();
     throw Exception("could not find a territory for player %d", playerId);
 }
-//------------------------------------------------------------------------------
-// GET MAXIMUM NUMBER OF PLAYER
-//------------------------------------------------------------------------------
-const size_t& Map::getMaximumNumberOfPlayers() const {
-    return maximumNumberOfPlayers;
-}
-//------------------------------------------------------------------------------
-// GET MAP MATRIX
-//------------------------------------------------------------------------------
-const mapMatrix& Map::getMapMatrix() const {
-    return gameMap;
-}
-//------------------------------------------------------------------------------
-// GET QUANTITY OF TERRITORIES
-//------------------------------------------------------------------------------
-size_t Map::getQuantityOfTerritories() const {
-    return territories.size();
-}
-//------------------------------------------------------------------------------
-// GET TERRITORIES
-//------------------------------------------------------------------------------
+
 const std::vector<Territories*>& Map::getTerritories() {
     return territories.getList();
 }
-// *****************************************************************************
-// ********************************PRIVATE**************************************
-// *****************************************************************************
-//------------------------------------------------------------------------------
-// CREATE STONE
-//------------------------------------------------------------------------------
-void Map::createStone(Node &position) {
+
+void Map::createObject(Node &position, size_t type, const object_t &attr) {
     size_t id = ++objectsQuant;
-    Stone* object = new Stone(position, id);
-    object->addObjectToNodes(*this);
+    auto* object = new Object(position, id, type);
+    object->addObjectToNodes();
+    object->addAttributes(attr, *this);
     objects(id, object);
-    solids(id, object);
 }
-//------------------------------------------------------------------------------
-// CREATE ICE BLOCKS
-//------------------------------------------------------------------------------
-void Map::createIceBlocks(Node &position) {
-    size_t id = ++objectsQuant;
-    IceBlock* object = new IceBlock(position, id);
-    object->addObjectToNodes(*this);
-    objects(id, object);
-    solids(id, object);
-}
-//------------------------------------------------------------------------------
-// CREATE FORT
-//------------------------------------------------------------------------------
-void Map::createFort(Node &position, size_t tecnologyLevel) {
-    maximumQuantityOfPlayers++;
-    size_t id = ++objectsQuant;
-    // Este new sera destruido en la clase nodo.
-    Fort* fort = new Fort(position, id, tecnologyLevel);
-    fort->addObjectToNodes(*this);
-    //--------------------------------------------------------------------------
-    Node* flagPosition;
-    flagPosition = fort->getFlagNode();
-    size_t idFlag = ++objectsQuant;
-    // Este new sera destruido en la clase nodo.
-    Flag* flag = new Flag(*flagPosition, idFlag);
-    objects(idFlag, flag);
-    flags(idFlag, flag);
-    fort->addFlag(*flag);
-    //--------------------------------------------------------------------------
-    objects(id, fort);
-    buildings(id, fort);
-}
-//------------------------------------------------------------------------------
-// CREATE ROBOT FACTORIE
-//------------------------------------------------------------------------------
-void Map::createRobotFactorie(
-        Node &position, size_t tecnologyLevel) {
-    size_t id = ++objectsQuant;
-    RobotFactory* object;
-    object = new RobotFactory(position, id, tecnologyLevel);
-    object->addObjectToNodes(*this);
-    objects(id, object);
-    buildings(id, object);
-}
-//------------------------------------------------------------------------------
-// CREATE VEHICLE FACTORIE
-//------------------------------------------------------------------------------
-void Map::createVehicleFactorie(
-        Node &position, size_t tecnologyLevel) {
-    size_t id = ++objectsQuant;
-    VehicleFactory* object;
-    object = new VehicleFactory(position, id, tecnologyLevel);
-    object->addObjectToNodes(*this);
-    objects(id, object);
-    buildings(id, object);
-}
-//------------------------------------------------------------------------------
-// CREATE FLAG
-//------------------------------------------------------------------------------
-void Map::createFlag(Node& position) {
-    size_t id = ++objectsQuant;
-    Flag* object = new Flag(position, id);
-    object->addObjectToNodes(*this);
-    objects(id, object);
-    flags(id, object);
-}
-//------------------------------------------------------------------------------
-// CREATE WOODEN BRIDGE
-//------------------------------------------------------------------------------
-void Map::createWoodenBridge(Node& position) {
-    size_t id = ++objectsQuant;
-    WoodenBridge* object = new WoodenBridge(position, id);
-    object->addObjectToNodes(*this);
-    objects(id, object);
-    bridges(id, object);
-}
-//------------------------------------------------------------------------------
-// CREATE CONCRETE BRIDGE
-//------------------------------------------------------------------------------
-void Map::createConcreteBridge(Node &position) {
-    size_t id = ++objectsQuant;
-    ConcreteBridge* object = new ConcreteBridge(position, id);
-    object->addObjectToNodes(*this);
-    objects(id, object);
-    bridges(id, object);
-}
-//------------------------------------------------------------------------------
-// ADD NODES TO TERRITORY
-//------------------------------------------------------------------------------
+
 void Map::addNodesToTerritory(
         parsedLine_t aNode, Territories *aTerritory) {
     uint32_t x = aParser.stringToUint32_t(aNode[0]);
     uint32_t y = aParser.stringToUint32_t(aNode[1]);
     aTerritory->addNode(*getNode(x, y));
 }
-//------------------------------------------------------------------------------
-// ADD OBJECT
-//------------------------------------------------------------------------------
+
 void Map::addObject(parsedLine_t object) {
     std::string objectType = object[0];
     Node* position = nullptr;
-    if (objectType == FORT) {
-        size_t tecnologyLevel = aParser.stringToSize_t(object[1]);
-        object.erase(object.begin());
-        object.erase(object.begin());
-        position = getPosition(object);
-        createFort(*position, tecnologyLevel);
-    } else if (objectType == VEHICLE_FACTORY) {
-        size_t tecnologyLevel = aParser.stringToSize_t(object[1]);
-        object.erase(object.begin());
-        object.erase(object.begin());
-        position = getPosition(object);
-        createVehicleFactorie(*position, tecnologyLevel);
-    } else if (objectType == ROBOT_FACTORY) {
-        size_t tecnologyLevel = aParser.stringToSize_t(object[1]);
-        object.erase(object.begin());
-        object.erase(object.begin());
-        position = getPosition(object);
-        createRobotFactorie(*position, tecnologyLevel);
-    } else if (objectType == STONE) {
-        object.erase(object.begin());
-        position = getPosition(object);
-        createStone(*position);
-    } else if (objectType == ICE_BLOCK) {
-        object.erase(object.begin());
-        position = getPosition(object);
-        createIceBlocks(*position);
-    } else if (objectType == WOODEN_BRIDGE) {
-        object.erase(object.begin());
-        position = getPosition(object);
-        createWoodenBridge(*position);
-    } else if (objectType == CONCRETE_BRIDGE) {
-        object.erase(object.begin());
-        position = getPosition(object);
-        createConcreteBridge(*position);
-    } else if (objectType == FLAG) {
-        object.erase(object.begin());
-        position = getPosition(object);
-        createFlag(*position);
-    } else {
-        throw Exception(
-                "THE TYPE OF OBJECT: '%s' DOES NOT EXIST", objectType.c_str());
+    size_t type = objectsNames.at(objectType);
+    object_t attributes;
+    if (object.size() == 4) {
+        attributes("techLevel", aParser.stringToSize_t(object[3]));
     }
+    position = getPosition(object);
+    createObject(*position, type, attributes);
 }
-//------------------------------------------------------------------------------
-// CREATE NODE
-//------------------------------------------------------------------------------
+
 void Map::createNode(parsedLine_t node) {
     uint32_t x = aParser.stringToUint32_t(node[0]);
     uint32_t y = aParser.stringToUint32_t(node[1]);
@@ -390,9 +180,7 @@ void Map::createNode(parsedLine_t node) {
                 "THE TYPE OF NODE: '%s' DOES NOT EXIST", nodeType.c_str());
     }
 }
-//------------------------------------------------------------------------------
-// CREATE MAP DIMENSIONS
-//------------------------------------------------------------------------------
+
 void Map::setDimensions(parsedLine_t dimensions) {
     width = aParser.stringToUint32_t(dimensions[0]);
     height = aParser.stringToUint32_t(dimensions[1]);
@@ -406,26 +194,20 @@ void Map::setDimensions(parsedLine_t dimensions) {
         gameMap.push_back(vecY);
     }
 }
-//------------------------------------------------------------------------------
-// CREATE NEW TERRITORY
-//------------------------------------------------------------------------------
+
 Territories* Map::createNewTerritory() {
     size_t id = territories.size() + 1;
-    Territories* aTerritory = new Territories(id);
+    auto* aTerritory = new Territories(id);
     territories(id, aTerritory);
     return aTerritory;
 }
-//------------------------------------------------------------------------------
-// GET BUILDING POSITIONS
-//------------------------------------------------------------------------------
+
 Node* Map::getPosition(const parsedLine_t& object) {
-    uint32_t x = aParser.stringToUint32_t(object[0]);;
-    uint32_t y = aParser.stringToUint32_t(object[1]);
+    uint32_t x = aParser.stringToUint32_t(object[1]);
+    uint32_t y = aParser.stringToUint32_t(object[2]);
     return getNode(x, y);
 }
-//------------------------------------------------------------------------------
-// ADD ADJACENTS TO ALL NODES
-//------------------------------------------------------------------------------
+
 void Map::addAdjacentToAllNodes() {
     if (adjacentsAdded) return;
     for (uint32_t x = 0; x < width; x++) {
@@ -435,4 +217,12 @@ void Map::addAdjacentToAllNodes() {
     }
     adjacentsAdded = true;
 }
-//------------------------------------------------------------------------------
+
+size_t Map::getNewObjectId() {
+    objectsQuant++;
+    return objectsQuant;
+}
+
+void Map::addObject(Object *anObject) {
+    objects(anObject->getId(), anObject);
+}
--- server/maps/map.h
+++ server/maps/map.h
@@ -1,8 +1,6 @@
-// "Copyright [2017] <Copyright SebastianBlanco>"
-//------------------------------------------------------------------------------
 #ifndef MAP_H
 #define MAP_H
-//------------------------------------------------------------------------------
+
 class Node;
 class Object;
 class Buildings;
@@ -11,19 +9,20 @@ class Territories;
 class Bridges;
 class Solids;
 class Flag;
-//------------------------------------------------------------------------------
+
 #include <string>
 #include <vector>
 #include <stdint-gcc.h>
 #include "../../libs/Dicc.h"
 #include "../../libs/Parser.h"
-//------------------------------------------------------------------------------
+#include "../settings/settings.h"
+
 typedef Dicc<size_t, Object*> objectsMap;
 typedef std::vector<std::vector<Node*>> mapMatrix;
 typedef std::vector<std::vector<std::string>> fileParsed;
 typedef std::vector<Node*> position_t;
 typedef std::vector<std::string> parsedLine_t;
-//------------------------------------------------------------------------------
+
 class Map {
  public :
     uint32_t height;
@@ -33,84 +32,34 @@ class Map {
     std::vector<std::vector<Node*>> gameMap;
     objectsMap objects;
     Dicc<size_t, Buildings*> buildings;
-    Dicc<size_t, Bridges*> bridges;
-    Dicc<size_t, Solids*> solids;
-    Dicc<size_t, Flag*> flags;
     Dicc<size_t, Territories*> territories;
     size_t objectsQuant;
     size_t maximumNumberOfPlayers;
     Parser aParser;
     bool adjacentsAdded;
  public :
-    //--------------------------------------------------------------------------
     explicit Map(char* mapFileName);
-    //--------------------------------------------------------------------------
     ~Map();
-    //--------------------------------------------------------------------------
     Node* getNode(uint32_t x, uint32_t y)const;
-    //--------------------------------------------------------------------------
     bool belongs(uint32_t x, uint32_t y)const;
-    //--------------------------------------------------------------------------
-    bool belongs(const Node& aNode)const;
-    //--------------------------------------------------------------------------
     void addAdjacentTo(Node &aNode)const;
-    //--------------------------------------------------------------------------
     objectsMap& getObjects();
-    //--------------------------------------------------------------------------
     Object* getObject(size_t id);
-    //--------------------------------------------------------------------------
     Buildings* getBuilding(size_t id);
-    //--------------------------------------------------------------------------
-    Bridges* getBridge(size_t id);
-    //--------------------------------------------------------------------------
-    Solids* getSolid(size_t id);
-    //--------------------------------------------------------------------------
-    Flag* getFlag(size_t id);
-    //--------------------------------------------------------------------------
     Territories* getTerritory(size_t id);
-    //--------------------------------------------------------------------------
     void addInitialTerritoryToPlayer(Player& aPlayer);
-    //--------------------------------------------------------------------------
-    const size_t& getMaximumNumberOfPlayers()const;
-    //--------------------------------------------------------------------------
-    const mapMatrix& getMapMatrix()const;
-    //--------------------------------------------------------------------------
-    size_t getQuantityOfTerritories()const;
-    //--------------------------------------------------------------------------
     const std::vector<Territories*>& getTerritories();
-    //--------------------------------------------------------------------------
+    size_t getNewObjectId();
+    void addObject(Object* anObject);
  private :
-    //--------------------------------------------------------------------------
-    void createStone(Node &position);
-    //--------------------------------------------------------------------------
-    void createIceBlocks(Node &position);
-    //--------------------------------------------------------------------------
-    void createFort(Node &position, size_t tecnologyLevel);
-    //--------------------------------------------------------------------------
-    void createRobotFactorie(Node &position, size_t tecnologyLevel);
-    //--------------------------------------------------------------------------
-    void createVehicleFactorie(Node &position, size_t tecnologyLevel);
-    //--------------------------------------------------------------------------
-    void createFlag(Node &position);
-    //--------------------------------------------------------------------------
-    void createWoodenBridge(Node &position);
-    //--------------------------------------------------------------------------
-    void createConcreteBridge(Node &position);
-    //--------------------------------------------------------------------------
+    void createObject(Node &position, size_t type, const object_t &attr);
     void addNodesToTerritory(parsedLine_t aNode, Territories *aTerritory);
-    //--------------------------------------------------------------------------
     void addObject(parsedLine_t object);
-    //--------------------------------------------------------------------------
     void createNode(parsedLine_t node);
-    //--------------------------------------------------------------------------
     void setDimensions(parsedLine_t dimensions);
-    //--------------------------------------------------------------------------
     Territories* createNewTerritory();
-    //--------------------------------------------------------------------------
     Node* getPosition(const parsedLine_t &object);
-    //--------------------------------------------------------------------------
     void addAdjacentToAllNodes();
-    //--------------------------------------------------------------------------
 };
-//------------------------------------------------------------------------------
-#endif  // MAP_H
+
+#endif // MAP_H
