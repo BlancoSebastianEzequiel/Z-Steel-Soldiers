1c71ddde6af988887d8c5e47efedf3c68083a69f
sebastian <sebastian.e.blanco@gmail.com>
2019-07-20 20:17:20 -0300

    refactor building object. Now there are only object with different behaviors

--- server/tasks/taskCreateUnit.cpp
+++ server/tasks/taskCreateUnit.cpp
@@ -1,40 +1,31 @@
 #include <iostream>
 #include "taskCreateUnit.h"
 #include "../games/game.h"
-#include "../objects/buildings/buildings.h"
+#include "../objects/object.h"
 #include "../players/player.h"
 #include "../terrains/node.h"
-//------------------------------------------------------------------------------
-// SERVER TASK CREATE GRUNT CONSTRUCTOR
-//------------------------------------------------------------------------------
+
 TaskCreateUnit::TaskCreateUnit(
         Game &aGame, size_t idBuilding, size_t type):
         Task::Task(aGame), idBuilding(idBuilding), type(type) {
-    Buildings* building = aGame.gameMap.getBuilding(idBuilding);
+    Object* building = aGame.gameMap.getObject(idBuilding);
     size_t takenTerritories = building->getOwner()->getTakenTerritories();
-    waitingTime = building->getUnitManufacturingTime(takenTerritories, type);
+    waitingTime = building->manufacturingTime(takenTerritories, type);
 }
-//------------------------------------------------------------------------------
-// SERVER TASK CREATE GRUNT DESTRUCTOR
-//------------------------------------------------------------------------------
+
 TaskCreateUnit::~TaskCreateUnit() {}
-//------------------------------------------------------------------------------
-// IS RELOCABLE
-//------------------------------------------------------------------------------
+
 bool TaskCreateUnit::isRelocable() const {
     return false;
 }
-//------------------------------------------------------------------------------
-// EXECUTE
-//------------------------------------------------------------------------------
+
 void TaskCreateUnit::execute() {
-    size_t id = ++aGame.unitsQuant;
-    Buildings* building = aGame.gameMap.getBuilding(idBuilding);
-    uint32_t x = building->getCreationPosition()->x;
-    uint32_t y = building->getCreationPosition()->y;
+    size_t unitId = ++aGame.unitsQuant;
+    Object* building = aGame.gameMap.getObject(idBuilding);
+    uint32_t x = building->getWalkableNode()->x;
+    uint32_t y = building->getWalkableNode()->y;
     auto* owner = (Player *)building->getOwner();
-    owner->createUnit(x, y, id, type);
-    Unit* newUnit =  owner->getUnit(id);
-    aGame.addNewUnit(id, newUnit);
+    owner->createUnit(x, y, unitId, type);
+    Unit* newUnit =  owner->getUnit(unitId);
+    aGame.addNewUnit(unitId, newUnit);
 }
-//------------------------------------------------------------------------------
